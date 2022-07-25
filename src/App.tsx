c3eccc2d3ec0a6b2b51e02c0f3585ba3e6658f46
Marius <marius@example.com>
2022-07-25 08:59:40 +0300

    sdsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+interface HeaderProps {
+  title: string;
+  isLight?: boolean
+}
+
+function Header({title}: HeaderProps): JSX.Element {
+  return <header><h2>{title}</h2></header>
+}
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="container">
+      <Header title='TypeScript React' />
       <h1>Hello</h1>
      
     </div>
